1bf414fba0eef2e14f43609aebdad4c28054f363
aliciamonge <alismonge@gmail.com>
2022-05-27 15:18:56 +0200

    search engine built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer-app">Coded by Alicia</footer>
+      </div>
     </div>
   );
 }
