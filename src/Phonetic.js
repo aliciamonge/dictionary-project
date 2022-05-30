ec0bf134738240ce9c1ecbba37d9b1666a336d87
aliciamonge <alismonge@gmail.com>
2022-05-30 19:11:21 +0200

    Phonetics componen created

---
+++ src/Phonetic.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+        Listen🔊
+      </a>
+      <br /> {props.phonetic.text}
+    </div>
+  );
+}
