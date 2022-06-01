c97eea3161322312967f75067359649f72dc5d6e
aliciamonge <alismonge@gmail.com>
2022-06-01 23:17:49 +0200

    Styling 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import logo from "./logo.png";
+
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
+        <header className="App-header"> What are you looking for?</header>
         <main>
           <Dictionary />
         </main>
